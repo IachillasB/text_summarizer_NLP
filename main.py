126d7be05f482eff2e4cfb3ffd9d6cf16fb6d21f
IachillasB <iachillasb@gmail.com>
2023-08-31 08:59:23 +0200

    Completed training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from text_summarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from text_summarizer.logging import logger
 
 
@@ -30,6 +31,17 @@ stage_name = "Data transformation stage"
 try:
     logger.info(f">>>> stage {stage_name} started <<<<")
     data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>> stage {stage_name} complete <<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+stage_name = "Data model trainer stage"
+try:
+    logger.info(f">>>> stage {stage_name} started <<<<")
+    data_transformation = ModelTrainerTrainingPipeline()
     data_transformation.main()
     logger.info(f">>>> stage {stage_name} complete <<<<\n\nx========x")
 except Exception as e:
