62b64d420e72faa65321ccd960ba1e5fa27932d1
IachillasB <iachillasb@gmail.com>
2023-08-31 08:58:11 +0200

    Model trainer complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from text_summarizer.logging import logger
 
 
@@ -7,6 +9,28 @@ try:
     logger.info(f">>>> stage {stage_name} started <<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> stage {stage_name} complete <<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+stage_name = "Data validation stage"
+try:
+    logger.info(f">>>> stage {stage_name} started <<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>> stage {stage_name} complete <<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+stage_name = "Data transformation stage"
+try:
+    logger.info(f">>>> stage {stage_name} started <<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>> stage {stage_name} complete <<<<\n\nx========x")
 except Exception as e:
     logger.exception(e)
